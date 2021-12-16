40a6e6e52b801f8dd011135f0bb9ee0bd3c6e5d9
Creatorpravin <creatorpravin@gmail.com>
2021-12-16 13:03:26 +0530

    body

---
+++ chapter 1/Console_Error.js
@@ -0,0 +1,11 @@
+let fuel = 99;
+function launch_rocket(){
+    function warning_mgs(){
+        console.error("Not enough fuel.");  
+    }
+}
+if(fuel <= 100)
+{
+    //looks everything is fine
+}
+launch_rocket();
