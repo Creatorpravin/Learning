6892942b5dd6e5f117fc0e46bb4e59d4bbf94fcc
Creatorpravin <creatorpravin@gmail.com>
2021-12-20 13:10:50 +0530

    4-Chapter completed

---
+++ chapter 4/expression.js
@@ -0,0 +1,6 @@
+1+1; //2
+let f = function(){return 1}; //undefined
+f(); //1
+var y=10; //undefined
+var x=(y>0?y:-y); //undefined
+console.log(x); //  10
---
+++ chapter 4/statement.js
@@ -0,0 +1,11 @@
+let x = 20,y,z;
+x;
+y;
+y=20;
+z=x+y;
+if(y>=0){
+    x=y;
+}else{
+x=-y;
+}
+console.log(x,y,z);
