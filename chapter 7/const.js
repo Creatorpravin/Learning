289b1547c007fb925c48974dfbf06d98a7408ef8
Creatorpravin <creatorpravin@gmail.com>
2021-12-22 17:49:37 +0530

    chapter 7

---
+++ chapter 7/const.js
@@ -0,0 +1,7 @@
+/*let a;
+console.log(a);
+const b;//Uncaught SyntaxError: Missing initializer in const declaration
+const speed_of_light = 186000;
+speed_of_light = 1; //VM1126:2 Uncaught TypeError: Assignment to constant variable.
+    at <anonymous>:2:16
+*/
---
+++ chapter 7/const_array.js
@@ -0,0 +1,3 @@
+const A = [];
+A[0] = 5; //ok
+A = [2]; //Uncaught SyntaxError: Identifier 'A' has already been declared
