497ac69d59fd502ac137060979bf40b492a5ebcc
Creatorpravin <creatorpravin@gmail.com>
2021-12-23 13:37:49 +0530

    string

---
+++ chapter 8/logical.js
@@ -0,0 +1,4 @@
+(5<1 && 3>2)//false AND both must be satisfied
+1==1 || 2==2//true OR any one satisfies 
+!true //false NOT EQUAL TO
+!(1 == 2) // true
---
+++ chapter 8/type_of.js
@@ -0,0 +1,10 @@
+typeof 10;//number
+typeof 10n;//bigint
+typeof 'text';//string
+typeof NaN;//number
+typeof true;//boolean
+typeof [];//object
+typeof {};//object
+typeof Object; //function
+typeof new Object();//object
+typeof null;//object
