d6a0aaad562826d91574b86e60c50436bac57b47
Creatorpravin <creatorpravin@gmail.com>
2022-06-04 10:00:54 +0530

    python

--- python/Rough_task/get_interface_ip.py
+++ python/Rough_task/get_interface_ip.py
@@ -1,7 +1,18 @@
+from asyncio import subprocess
 import os
+import subprocess
 
 interface = 'wlp0s20f3'
+cmd = "ip addr show "+interface
 
-ip = os.popen("ip addr show "+interface).read().split("inet ")[1].split("brd")[0]
 
-print(ip)
+status = os.popen(cmd).read().split("state ")[1].split(" group")[0]
+
+#print(status)
+
+if status=="DOWN":
+  print("Interface is",status)
+else:
+  ip = os.popen(cmd).read().split("inet ")[1].split("brd")[0]
+  print(ip)
+
