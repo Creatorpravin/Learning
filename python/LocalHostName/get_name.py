b53fdb7f51ec7a55320c4782816d6e5e72fe2e6b
Creatorpravin <creatorpravin@gmail.com>
2022-07-08 11:27:25 +0530

    python

--- python/LocalHostName/get_name.py
+++ python/LocalHostName/get_name.py
@@ -2,10 +2,11 @@ file = open("dnsmasq.leases","r")
 content = []
 file_content = file.readlines()
 
+
 for lines in file_content[0: ]:
      content.append(lines.split())
 
 #print(content[12][3])
 for data in content:
-    if data[2] == "192.168.10.32": 
+    if data[2] == "192.168.10.162": 
       print(data[3])
