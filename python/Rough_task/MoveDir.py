57242e0b7028094b7aeb963fed9d26ee31d8a88c
Creatorpravin <creatorpravin@gmail.com>
2022-08-11 10:26:45 +0530

    python

---
+++ python/Rough_task/MoveDir.py
@@ -0,0 +1,6 @@
+import shutil
+
+source = "/home/praveen/telegraf/custom_scripts/hii.xml"
+destination = "/home/praveen/telegraf"
+
+shutil.move( source , destination )
--- python/Update_CPE_Code/main.py
+++ python/Update_CPE_Code/main.py
@@ -1,10 +1,13 @@
+from logging import exception
+import re
 import subprocess
 import json
 import os
 from sys import stderr
+import shutil
 
 
-def installation(installation_json):
+def main(installation_json):
 
     if os.path.exists(installation_json) is True:
         with open(file=installation_json, mode="r") as install_json:
@@ -13,27 +16,58 @@ def installation(installation_json):
             for i in key_list:
                 if (len(json_data[i])) > 1:
                     for j in range(len(json_data[i])):
-                        #print(key_list[0])
+                        # print(key_list[0])
                         if (len(json_data[i])) > 3:
-                            print(json_data[i][j]["package"])
+                            return_value = shell_execution(json_data[i][j]["package"])
+                            if return_value==True:
+                                print(json_data[i][j]["package"]+" Installed successfully")
+                            else:
+                                print(json_data[i][j]["package"]+" Installation failed")
                         else:
-                            print(json_data[i][j]["package"])
+                            print(json_data[i][j]["file_name"])
 
                 else:
                     print(json_data[i])
 
 
 def shell_execution(shell_file_name):
+    print(shell_file_name+" is installing")
     execution_command = "./"+shell_file_name
     print(execution_command)
-    execution = subprocess.Popen(execution_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    result_out = execution.communicate()
-    print(result_out)
+    execution = subprocess.Popen(execution_command, shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    result_out, result_err = execution.communicate()
+
+    if execution.returncode == 0:
+        print("out: {0}". format(result_out))
+        print(shell_file_name+" install successfully")
+    else:
+        print("error: {0}". format(result_err))
+        print(shell_file_name+" installation failed")
+        return False
+
+    return True
 
 
+def move_files_in_directory(source_directory_path, destination_directory_path):
 
+    if os.path.isdir(source_directory_path) != True:
+        print("{} path is not a valid directory".format(source_directory_path))
+        return False
+
+    if os.path.isdir(destination_directory_path) != True:
+        print("{} path is not a valid directory".format(
+            destination_directory_path))
+        return False
+
+    try:
+        shutil.move(source_directory_path, destination_directory_path)
+
+    except Exception as exeception:
+        print(exception)
+        return False
 
 
 if __name__ == "__main__":
-    installation("installation.json")
-    shell_execution("aptupdate")
+    main("installation.json")
+
