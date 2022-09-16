05fc656e5b3ced717c90194db266ee8dce83aeff
Creatorpravin <creatorpravin@gmail.com>
2022-09-16 15:42:58 +0530

    python3

--- python/LocalHostName/active_hosts.py
+++ python/LocalHostName/active_hosts.py
@@ -1,80 +1,110 @@
+#!/usr/bin/env python
+
+from logging import exception
 import subprocess
 import json
 import os
 
-dnsmasq_contents = []
-ip_neigh = "ip -json neigh show"
-reachable = ["REACHABLE"]
+DEFAULT_SYSTEM_CONFIGURATION_FILE = "/etc/telegraf/custom_scripts/SystemConfiguration.json"
 
 
-def get_hostname(file_path):
-    result = subprocess.Popen(ip_neigh, shell=True, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE, universal_newlines=True)
-
-    json_out, json_err = result.communicate()
-    if os.path.exists(file_path) is True:
-        with open(file=file_path, mode="r") as dnsmasq_data:
-            dnsmasq_contents = dnsmasq_data.readlines()
-        for lines in dnsmasq_contents[0:]:
-            dnsmasq_contents.append(lines.split())
-    else:
-        ipneig_json_1 = json.loads(json_out)
-        for ipneig_data_1 in ipneig_json_1:
-            print(int(ipneig_data_1['dst']))
-            get_nbtscan_1 = nbtscan(ipneig_data_1['dst'])
-
-            if get_nbtscan_1 == False:
-                print("active_hosts,interface=" +
-                      ipneig_data_1['dev']+",ip_address="+ipneig_data_1['dst']+' host_name=\"-\",status="REACHABLE"')
-            elif get_nbtscan_1 == "<unknown>":
-                print("active_hosts,interface=" +
-                      ipneig_data_1['dev']+",ip_address="+ipneig_data_1['dst']+' host_name=\"-\",status="REACHABLE"')
-            else:
-                print("active_hosts,interface=" +
-                      ipneig_data_1['dev']+",ip_address="+ipneig_data_1['dst']+" host_name="+'"'+get_nbtscan_1+'",status="REACHABLE"')
-        return
-    try:
-        ipneig_json = json.loads(json_out)
-        for ipneig_data in ipneig_json:
-            if ipneig_data['state'] == reachable:
-                for dnsmasq_data in dnsmasq_contents:
-                    if dnsmasq_data[2] == ipneig_data['dst']:
-                        if dnsmasq_data[3] != '*':
-                            print("active_hosts,interface=" +
-                                  ipneig_data['dev']+",ip_address="+dnsmasq_data[2]+" host_name="+'"'+dnsmasq_data[3]+'",status="REACHABLE"')
-                        elif dnsmasq_data[3] == '*':
-                            get_nbtscan = nbtscan(dnsmasq_data[2])
-
-                            if get_nbtscan == False:
-                                print("active_hosts,interface=" +
-                                      ipneig_data['dev']+",ip_address="+dnsmasq_data[2]+' host_name=\"-\",status="REACHABLE"')
-                            elif get_nbtscan == "<unknown>":
-                                print("active_hosts,interface=" +
-                                      ipneig_data['dev']+",ip_address="+dnsmasq_data[2]+' host_name=\"-\",status="REACHABLE"')
+def get_hostname():
+    LAN_INTERFACES = lan_interface_list()
+    if len(LAN_INTERFACES):
+        try:
+            for lan_data in LAN_INTERFACES:
+#                print(lan_data)
+                ip_neigh = "ip -json neigh show dev " + lan_data + " nud reachable"
+                result = subprocess.Popen(ip_neigh, shell=True, stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE, universal_newlines=True)
+                json_out, json_err = result.communicate()
+                if result.returncode == 0:
+#                  print(json_out)
+                    ipneig_json = json.loads(json_out)
+                    if len(ipneig_json) == 0:
+#                       print(ipneig_json)
+                       # print("active_hosts,interface=" +
+                        #      lan_data+",ip_address=0 "+'host_name=\"-\",status=\"-\"')
+                       pass
+                    else:
+                        for ipneigh_data in ipneig_json:
+                            get_nslookup = nslookup(ipneigh_data['dst'])
+                            if get_nslookup == False:
+                                get_nbtscan = nbtscan(ipneigh_data['dst'])
+                                if get_nbtscan == False:
+                                    print("active_hosts,interface=" +
+                                          lan_data+",ip_address="+ipneigh_data['dst']+' host_name=\"-\",status="REACHABLE"')
+                                elif get_nbtscan == "<unknown>":
+                                    print("active_hosts,interface=" +
+                                          lan_data+",ip_address="+ipneigh_data['dst']+' host_name=\"-\",status="REACHABLE"')
+                                else:
+                                    print("active_hosts,interface=" +
+                                          lan_data+",ip_address="+ipneigh_data['dst']+" host_name="+'"'+get_nbtscan+'",status="REACHABLE"')
                             else:
                                 print("active_hosts,interface=" +
-                                      ipneig_data['dev']+",ip_address="+dnsmasq_data[2]+" host_name="+'"'+get_nbtscan+'",status="REACHABLE"')
+                                      lan_data+",ip_address="+ipneigh_data['dst']+" host_name="+'"'+get_nslookup.replace(".", "")+'",status="REACHABLE"')
+                else:
+                   # print("active_hosts,interface=" +
+                    #      lan_data+",ip_address=0 "+'host_name=\"-\",status=\"-\"')
+                    pass
+        except Exception as exception:
+            pass
+           # print("active_hosts,interface=-"+",ip_address=0 " +
+            #      'host_name=\"-\",status=\"-\"')
+    else:
+        pass
+        #print("active_hosts,interface=-"+",ip_address=0 " +
+         #     'host_name=\"-\",status=\"-\"')
 
+
+def lan_interface_list():
+    try:
+        if os.path.exists(DEFAULT_SYSTEM_CONFIGURATION_FILE) is True:
+            with open(file=DEFAULT_SYSTEM_CONFIGURATION_FILE, mode="r") as system_config:
+                system_config_json = json.loads(system_config.read())
+                lan_interfaces = system_config_json["system_information"]["lan_interfaces"]
+                return lan_interfaces
+        else:
+            lan_interfaces = []
+            return []
     except Exception as exceptions:
-        print(exceptions)
+        lan_interfaces = []
+    return []
 
 
 def nbtscan(ip):
+    nbtscan_command = "nbtscan -r "+ip
+    try:
+        nbtscan_communication = subprocess.Popen(nbtscan_command, shell=True, stdout=subprocess.PIPE,
+                                                 stderr=subprocess.PIPE, universal_newlines=True)
+        nbtscan_out, nbtscan_err = nbtscan_communication.communicate()
+        nbtscan_list = nbtscan_out.split()
+        if nbtscan_communication.returncode == 0:
+            if len(nbtscan_list) > 18:
+                return(nbtscan_list[18])
+            else:
+                return False
+        else:
+            return False
+    except Exception as exception:
+        return False
 
-    nbtscan_command = "sudo nbtscan -r "+ip
 
-    nbtscan_communication = subprocess.Popen(nbtscan_command, shell=True, stdout=subprocess.PIPE,
-                                             stderr=subprocess.PIPE, universal_newlines=True)
-
-    nbtscan_out, nbtscan_err = nbtscan_communication.communicate()
-    nbtscan_list = nbtscan_out.split()
-    if len(nbtscan_list) > 18:
-        #        print(nbtscan_list)
-        return(nbtscan_list[18])
-    else:
-        # print(nbtscan_list)
+def nslookup(ip):
+    nslookup_command = "nslookup " + ip + " 127.0.0.1"
+    try:
+        nslookup_communication = subprocess.Popen(
+            nslookup_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        nslookup_out, nslookup_err = nslookup_communication.communicate()
+        nslookup_list = nslookup_out.split()
+        if nslookup_communication.returncode == 0:
+            result = nslookup_list[len(nslookup_list) - 1]
+            return result
+        else:
+            return False
+    except Exception as exception:
         return False
 
 
 if __name__ == "__main__":
-    get_hostname("/var/lib/misc/dnsmasq.leases")
+    get_hostname()
