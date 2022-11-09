59319b6d810a5cd34c04044b175c2e646f5168d5
Creatorpravin <creatorpravin@gmail.com>
2022-11-09 17:59:30 +0530

    Python

--- python/Rough_task/removeAllDirInDir.py
+++ python/Rough_task/removeAllDirInDir.py
@@ -2,12 +2,16 @@ import os
 import glob
 import shutil
 
-DIRECTORY = '/home/ragu/FactoryReset'
-files = os.listdir(DIRECTORY)
-for f in files:
-    if os.path.isdir(DIRECTORY +'/'+ str(f)): #check that is a dir
-        print(f)
-        shutil.rmtree(DIRECTORY +'/'+ str(f)) 
+DIRECTORY = '/home/praveen/actions'
+FOLDER_NAME = "tions"
+LIST_DIRECTORY = '/home/praveen'
+# shutil.rmtree(DIRECTORY)
+# print("END")
+# files = os.listdir(DIRECTORY)
+# for f in files:
+#     if os.path.isdir(DIRECTORY +'/'+ str(f)): #check that is a dir
+#         print(f)
+#         shutil.rmtree(DIRECTORY +'/'+ str(f)) 
     #elif os.path.isfile(DIRECTORY + "/" + str(f)):
      #   os.remove(DIRECTORY + '/' + str(f))
 
@@ -15,3 +19,7 @@ for f in files:
 #shutil.rmtree('/home/ragu/FactoryReset') #hard delete the folder
 
 #list( map( os.unlink, (os.path.join( '/home/ragu/Definitions',f) for f in os.listdir('/home/ragu/Definitions')) ) ) #hard delete all files in a dir  
+
+for filename in os.listdir(LIST_DIRECTORY):
+    if FOLDER_NAME in filename:
+        shutil.rmtree(LIST_DIRECTORY + "/" +filename)
