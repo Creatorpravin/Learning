2e15aa55187f23739ae59fc21508264c7603474b
Creatorpravin <creatorpravin@gmail.com>
2022-11-18 18:04:36 +0530

    JSON

--- python/Rough_task/geo_locator/ip_locator.py
+++ python/Rough_task/geo_locator/ip_locator.py
@@ -1,12 +1,26 @@
-import geocoder as geo
+# import geocoder as geo
 
-ip_addr = "13.107.6.171"
+# ip_addr = "1.1.1.1"
 
-ip = geo.ip(ip_addr)
+# ip = geo.ip(ip_addr)
 
-print(ip.city)
-print(ip.country)
-print(ip.latlng)
-print(ip)
+# print(type(ip.city))
+# if ip.city == None:
+#     print("None")
+# else:
+#     print(ip.city)
+import subprocess
+import re
 
+cmd = "geoiplookup 15.207.189.46"
 
+ip_add_location = subprocess.Popen(
+    cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+location, location_err = ip_add_location.communicate()
+if ip_add_location.returncode == 0:
+    if "IP Address not found" in location:
+        print("IP Address not found")
+    else:
+        location_li = re.sub(r'[^\w]', ' ', location).split(" ")
+        print("country = ", location_li[4])
