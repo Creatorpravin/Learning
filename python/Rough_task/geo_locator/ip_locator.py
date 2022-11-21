f2b17adac7515447a59b72e9e2383cb3f1b7d565
Creatorpravin <creatorpravin@gmail.com>
2022-11-21 13:02:21 +0530

    test

--- python/Rough_task/geo_locator/ip_locator.py
+++ python/Rough_task/geo_locator/ip_locator.py
@@ -19,6 +19,7 @@ ip_add_location = subprocess.Popen(
     cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
 location, location_err = ip_add_location.communicate()
+
 if ip_add_location.returncode == 0:
     if "IP Address not found" in location:
         print("IP Address not found")
