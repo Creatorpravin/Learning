85a5da34bc3c9c3f5edb941d2bfef6bcaee5223b
Creatorpravin <creatorpravin@gmail.com>
2022-11-23 10:42:17 +0530

    Django

--- python/Rough_task/list_file_dir.py
+++ python/Rough_task/list_file_dir.py
@@ -7,3 +7,14 @@ for filename in os.listdir(FILE_PATH):
     if PDF_SUFIX in filename:
         filepath = FILE_PATH + os.path.sep + filename
         print(filename)      
+
+FILE_STUFFS = os.listdir(FILE_PATH)
+
+if "test.p" in FILE_STUFFS:
+    print(True)
+else:
+    print(False)
+
+# Check the file exist exists or not
+if os.path.isfile(path=FILE_PATH + "/Employee_detail.pdf") == True:
+    print(True)
