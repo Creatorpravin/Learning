6123fac20c9b826715aa1d3ebcf49761349d5bb4
Creatorpravin <creatorpravin@gmail.com>
2022-11-25 09:37:53 +0530

    Django

--- python/Django/myproject/myworld/members/views.py
+++ python/Django/myproject/myworld/members/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  # template = loader.get_template('myfirst.html')
+  # return HttpResponse(template.render())
+  # mymembers = Members.objects.all().values()
+  # output = ""
+  # mem = Members(firstname="Rahul", lastname="Raj") # add value on table 
+  # mem.save()
+  # for x in mymembers:
+  #   output += "Firstname:"+ x["firstname"] + " Lastname:" + x["lastname"] + "\t"
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+
+  context = {
+    'mymembers' : mymembers
+  }
+  
+  return HttpResponse(template.render(context, request)) # send the object to html template
