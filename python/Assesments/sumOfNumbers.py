b36261bf8ba1715788ab2496ce48083946a78d68
Creatorpravin <creatorpravin@gmail.com>
2023-01-02 15:47:30 +0530

    Assesments

---
+++ python/Assesments/sumOfNumbers.py
@@ -0,0 +1,7 @@
+J=[1,2,3,4,5,6,7,8,9,10,96,97,98,99,100]
+res=[]
+
+for i in range(0,len(J),5):   #range(start,end,increment by)
+    res.append(sum(J[i:i+5])) #sum(listOfRange(start,end))
+
+print(res) #[15, 40, 490]
