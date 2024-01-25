2c9997ea28d920ec13dea96d5373407f4421c3a8
PraveenYavar <praveenkumar@yavar.in>
2024-01-25 21:37:34 +0530

    DJANGO UPDATE

--- python/Django-2023/home/views.py
+++ python/Django-2023/home/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView,LogoutView
 
 from django.views.generic import TemplateView
 
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+    
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 class  HomeView(TemplateView):
     template_name = 'home/welcome.html'
--- python/Django-2023/notes/views.py
+++ python/Django-2023/notes/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import Http404
+from django.http.response import Http404,HttpResponseRedirect
 from django.views.generic import CreateView,DetailView,ListView,UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -20,12 +23,24 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+        
      
-class NotesListView(ListView): #end point create
+class NotesListView(LoginRequiredMixin,ListView): #end point create
     model = Notes
     context_object_name = 'notes'
     template_name = "notes/notes_list.html" # don't need if we follow the proper name for the class
-
+    login_url = '/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = 'note' # it default handle the exception
